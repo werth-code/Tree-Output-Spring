3416978e8c191bfb172f45230f79b479344e08c1
Matt Werth <matthewwerth@gmail.com>
2021-02-06 21:19:52 -0500

    Starter For TOO

---
+++ src/test/java/werth/matt/SimpleSpring/SimpleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package werth.matt.SimpleSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
