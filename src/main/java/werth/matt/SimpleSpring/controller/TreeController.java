67917a97fcccf8d2efeb0445f11804ce97dbe6db
Matt Werth <matthewwerth@gmail.com>
2021-02-06 23:04:03 -0500

    Basic CRUD Operations Working

--- src/main/java/werth/matt/SimpleSpring/controller/TreeController.java
+++ src/main/java/werth/matt/SimpleSpring/controller/TreeController.java
@@ -1,9 +1,8 @@
 package werth.matt.SimpleSpring.controller;
 
+import org.springframework.web.bind.annotation.*;
 import werth.matt.SimpleSpring.model.Tree;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import werth.matt.SimpleSpring.service.TreeService;
 
 import java.util.List;
@@ -22,6 +21,26 @@ public class TreeController {
     @GetMapping("/trees")
     public List<Tree> getTrees() {
         return treeService.getTrees();
+    }
+
+    @GetMapping("/trees/{id}")
+    public Tree getTreeByID(@PathVariable String id) {
+        return treeService.getTreeByID(id);
+    }
+
+    @PostMapping("/trees")
+    public void addTree(@RequestBody Tree tree) {
+        treeService.addTree(tree);
+    }
+
+    @PutMapping("/trees/{id}")
+    public void updateTree(@RequestBody Tree tree, @PathVariable String id) {
+        treeService.updateTree(id, tree);
+    }
+
+    @DeleteMapping("/trees/{id}")
+    public void deleteTree(@PathVariable String id) {
+        treeService.removeTree(id);
     }
 
 }
