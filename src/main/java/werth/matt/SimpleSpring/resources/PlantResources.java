5b737654b2ad3758f1d87ccd4d80bf690fad04c9
Matt Werth <matthewwerth@gmail.com>
2021-02-16 18:20:08 -0500

    Adding @Annotation to bring into Spring

--- src/main/java/werth/matt/SimpleSpring/resources/PlantResources.java
+++ src/main/java/werth/matt/SimpleSpring/resources/PlantResources.java
@@ -5,14 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import werth.matt.SimpleSpring.model.PlantData;
 import werth.matt.SimpleSpring.model.PlantDataRequest;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/search")
 public class PlantResources {
@@ -25,6 +23,7 @@ public class PlantResources {
     @Autowired
     private RestTemplate restTemplate;
 
+    @CrossOrigin
     @GetMapping("/{searchTerm}")
     public PlantData[] getPlantInfo(@PathVariable String searchTerm) {
 
